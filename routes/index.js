420c145504623bb65147addb29d5e1bcb739d473
Adalia da Silva <ada.future.work@gmail.com>
2022-03-16 15:45:53 +0100

    Iteration 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 const router = require("express").Router();
+const UserModel = require('../models/User.model')
+const bcrypt = require('bcryptjs');
+const { genSalt } = require("bcrypt");
+const async = require("hbs/lib/async");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+/* GET signup page */
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+/* GET home page */
+router.post("/signup", async (req, res, next) => {
+  const {username, password} = req.body;
+
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = {
+    username,
+    password : hash,
+  }
+  await UserModel.create(user);
+  res.render('profile') // ,'welcome' {username}
+});
+
+
 
 module.exports = router;
